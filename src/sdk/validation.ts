2baa3eb41202e679e37cfeef201a72e9fbc53ca0
Marcos Passos <marcos@marcospassos.com>
2020-05-21 10:56:31 -0300

    Bump SDK version to 0.2.0-alpha.6

--- src/sdk/validation.ts
+++ src/sdk/validation.ts
@@ -1,9 +1,10 @@
 export * from '@croct/sdk/validation';
 export * from '@croct/sdk/error';
 export * from '@croct/sdk/validation/jsonType';
-export {default as MixedSchema} from '@croct/sdk/validation/mixedSchema';
-export {default as BooleanType} from '@croct/sdk/validation/booleanType';
 export {default as ArrayType} from '@croct/sdk/validation/arrayType';
+export {default as BooleanType} from '@croct/sdk/validation/booleanType';
+export {default as FunctionType} from '@croct/sdk/validation/functionType';
+export {default as MixedSchema} from '@croct/sdk/validation/mixedSchema';
 export {default as NullType} from '@croct/sdk/validation/nullType';
 export {default as NumberType} from '@croct/sdk/validation/numberType';
 export {default as ObjectType} from '@croct/sdk/validation/objectType';

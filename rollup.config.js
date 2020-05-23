e94909e8d192d9e886fcd26db28c29ece61c7145
Marcos Passos <marcos@marcospassos.com>
2020-05-23 20:30:49 -0300

    Replace CDN placeholders in bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,19 @@
 import resolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
 import typescript from 'rollup-plugin-typescript2';
 import tempDir from 'temp-dir';
 import {uglify} from 'rollup-plugin-uglify';
 import commonjs from '@rollup/plugin-commonjs';
 
-export default () => {
+export default (args) => {
+    if (args['config-cdn-prefix'] === undefined) {
+        throw new Error('The argument "config-cdn-prefix" is missing.');
+    }
+
+    if (args['config-cdn-suffix'] === undefined) {
+        throw new Error('The argument "config-cdn-suffix" is missing.');
+    }
+
     return [
         {
             input: 'src/index.ts',
@@ -26,6 +35,12 @@ export default () => {
                             module: "ES2015"
                         }
                     },
+                }),
+                replace({
+                    delimiters: ['<@', '@>'],
+                    'cdn-prefix': args['config-cdn-prefix'],
+                    'cdn-suffix': args['config-cdn-suffix'],
+
                 }),
                 uglify({
                     compress: {

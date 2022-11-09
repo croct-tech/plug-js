c0f1e695f72e8a9be1c5536d03fe4e0667ac7b5c
Marcos Passos <marcos@croct.com>
2022-11-08 21:04:49 -0300

    Mark payload as deprecated

--- src/fetch.ts
+++ src/fetch.ts
@@ -8,5 +8,9 @@ export type SlotId = keyof SlotMap extends never ? string : keyof SlotMap;
 export type SlotContent<I extends SlotId> = I extends keyof SlotMap ? SlotMap[I] : JsonObject;
 
 export type FetchResponse<I extends SlotId, P extends JsonObject = JsonObject> = {
+    /**
+     * @deprecated Use `content` instead.
+     */
     payload: SlotContent<I> & P,
+    content: SlotContent<I> & P,
 };

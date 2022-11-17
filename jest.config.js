8859be1f9da61e994707e329cecbdf55497047be
Marcos Passos <marcos@croct.com>
2022-11-16 21:09:27 -0300

    Fix package config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     testEnvironment: 'jsdom',
-    testMatch: ['**/test/**/*.test.ts'],
+    testMatch: ['<rootDir>/test/**/*.test.ts'],
     transformIgnorePatterns: [
         'node_modules/(?!@croct/.*)',
     ],

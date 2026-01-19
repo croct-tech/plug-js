fffa6bd70b07680cb6ddcc99c826937ceb2274f2
Marcos Passos <marcos@croct.com>
2026-01-19 13:33:05 -0300

    Update types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import {Configuration, Plug, GlobalPlug} from './plug';
+import {Configuration, Plug, GlobalPlug, FetchResponse} from './plug';
 
-export type {Configuration, Plug};
+export type {Configuration, Plug, FetchResponse};
 
 /* eslint-disable-next-line import/no-default-export -- Should be default export */
 export default GlobalPlug.GLOBAL;

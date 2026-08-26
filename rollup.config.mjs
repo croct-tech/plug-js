24f55c07d4ae36fb262ab5f9b5eddcfb9d6a503b
Marcos Passos <marcos@croct.com>
2026-08-26 16:00:51 -0300

    Remove leftover playground constants
    `PLAYGROUND_ORIGIN` and `PLAYGROUND_CONNECT_URL` were left behind when the
    playground plugin was removed in #350. Nothing has referenced them since,
    in this package or in plug-react, plug-next and application-ui-js, and the
    bundler already tree-shakes them out of the CDN bundle.
    
    Drop them from the constants, the release configuration, the bundle
    arguments and the workflow that passes them.
    
    Note that `constants` is reachable through the package exports, so this
    removes two public exports.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -14,14 +14,6 @@ export default args => {
         throw new Error('The argument "config-cdn-url" is missing.');
     }
 
-    if (args['config-playground-origin'] === undefined) {
-        throw new Error('The argument "config-playground-origin" is missing.');
-    }
-
-    if (args['config-playground-connect-url'] === undefined) {
-        throw new Error('The argument "config-playground-connect-url" is missing.');
-    }
-
     if (args['config-preview-widget-origin'] === undefined) {
         throw new Error('The argument "config-preview-widget-origin" is missing.');
     }
@@ -50,8 +42,6 @@ export default args => {
                     preventAssignment: true,
                     delimiters: ['<@', '@>'],
                     cdnUrl: args['config-cdn-url'],
-                    playgroundOrigin: args['config-playground-origin'],
-                    playgroundConnectUrl: args['config-playground-connect-url'],
                     previewWidgetOrigin: args['config-preview-widget-origin'],
                     previewWidgetUrl: args['config-preview-widget-url'],
                 }),

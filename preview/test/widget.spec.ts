5054cda076a7a4d694e4c1b2d38b51513fa82483
Winicius Kyutoku <winiciuskyutoku@gmail.com>
2026-09-24 17:59:33 -0300

    [WIP]

--- preview/test/widget.spec.ts
+++ preview/test/widget.spec.ts
@@ -180,34 +180,6 @@ test.describe('Preview widget', () => {
         await expect(page.locator('.options .title')).toHaveText(['Slot', 'Content', 'Locale']);
     });
 
-    test('should display the experience when previewing the slot timeline', async ({page}) => {
-        await open(page, {
-            previewMode: 'slotTimeline',
-            slot: 'Home banner',
-            experience: 'Experience',
-            experiment: 'Experiment',
-            audience: 'Audience',
-            variant: 'Variant',
-            locale: 'en-us',
-        });
-
-        const disclosure = page.locator('#disclosure');
-
-        await disclosure.click();
-
-        await expect(disclosure).toHaveAttribute('aria-expanded', 'true');
-
-        await expect(page.locator('#preview-experience')).toHaveText('Experience');
-
-        await expect(page.locator('#preview-experiment')).toHaveText('Experiment');
-
-        await expect(page.locator('#preview-audience')).toHaveText('Audience');
-
-        await expect(page.locator('#preview-content')).toHaveText('Variant');
-
-        await expect(page.locator('#preview-slot')).not.toBeAttached();
-    });
-
     test('should display the experiment default content if no variant is specified', async ({page}) => {
         await open(page, {
             experience: 'Experience',
